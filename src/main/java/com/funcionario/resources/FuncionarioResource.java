d74acbe92c0586203d7fa3a75db440d6049470cf
David Belfort <davidedysonn@gmail.com>
2026-05-11 21:44:57 -0300

    feat(funcionario): adicao do resource.

---
+++ src/main/java/com/funcionario/resources/FuncionarioResource.java
@@ -0,0 +1,62 @@
+package com.funcionario.resources;
+
+import com.funcionario.dtos.FuncionarioDTO;
+import com.funcionario.service.FuncionarioService;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@Path("/funcionarios")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class FuncionarioResource {
+
+    private final FuncionarioService funcionarioService;
+
+    public FuncionarioResource(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
+
+    @POST
+    public Response criar (@Valid FuncionarioDTO funcionarioDTO){
+        FuncionarioDTO funcionarioResponse = funcionarioService.criar(funcionarioDTO);
+        return Response
+                .status(Response.Status.CREATED)
+                .entity(funcionarioResponse)
+                .build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response atualizar (@PathParam("id") Long id, @Valid FuncionarioDTO funcionarioDTO){
+        FuncionarioDTO funcionarioResponse = funcionarioService.atualizar(id, funcionarioDTO);
+        return Response
+                .status(Response.Status.OK)
+                .entity(funcionarioResponse)
+                .build();
+    }
+
+    @GET
+    public List<FuncionarioDTO> listFuncionarios (){
+        return funcionarioService.listarFuncionarios();
+    }
+
+    @GET
+    @Path("/{id}")
+    public FuncionarioDTO buscarFuncionarioPorId (@PathParam("id") Long id){
+        return funcionarioService.buscarPorId(id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deletar (@PathParam("id") Long id){
+        funcionarioService.removerFuncionario(id);
+        return Response
+                .status(Response.Status.NO_CONTENT)
+                .build();
+    }
+}
