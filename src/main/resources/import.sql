fba93cdb56d7fb2a0e672fb13a6d7007b2e244d8
David Belfort <davidedysonn@gmail.com>
2026-05-11 22:21:50 -0300

    import(SQL): descomentado sql de funcionario para uso de seeds.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,8 +3,8 @@ VALUES
     (1, 'DAVID BELFORT', '12345678901'),
     (2, 'MARIA ROCHA', '98765432100');
 
--- INSERT INTO funcionarios (id, matricula, status, turno, pessoa_id)
--- VALUES
---     (1, '1234', true, 'MATUTINO', 1),
---     (2, '4321', true, 'NOTURNO', 1),
---     (3, '5678', false, 'VESPERTINO', 2);
+INSERT INTO funcionarios (id, matricula, status, turno, pessoa_id)
+VALUES
+    (1, '1234', false, 'MATUTINO', 1),
+    (2, '4321', true, 'NOTURNO', 1),
+    (3, '5678', false, 'VESPERTINO', 2);
