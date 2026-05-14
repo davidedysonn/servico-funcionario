dcf9dcf2518914b25c935151f7b3b5dbc1590ca7
David Belfort <davidedysonn@gmail.com>
2026-05-13 21:29:21 -0300

    feat(pessoa): adicionado o campo id para uma melhor visualizacao.

--- src/main/java/com/funcionario/dtos/PessoaDTO.java
+++ src/main/java/com/funcionario/dtos/PessoaDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record PessoaDTO(
 
+        Long id,
         @NotBlank(message = "Nome é obrigatório")
         @Size(max = 50, message = "Nome deve ter no máximo 50 caracteres")
         String nome,
