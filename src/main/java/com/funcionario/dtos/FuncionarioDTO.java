59a7ba5ae61e9325747b2d673b7865aa93e73cf1
David Belfort <davidedysonn@gmail.com>
2026-05-13 21:28:25 -0300

    fix(funcionario): dto notacao errada no campo de turno. Alterado notacao @NotBlank para @NotNull.

--- src/main/java/com/funcionario/dtos/FuncionarioDTO.java
+++ src/main/java/com/funcionario/dtos/FuncionarioDTO.java
@@ -2,15 +2,17 @@ package com.funcionario.dtos;
 
 import com.funcionario.enums.Turno;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record FuncionarioDTO(
 
+        Long id,
         @NotBlank(message = "A matrícula é obrigatória")
         @Size(min = 4, max = 4)
         String matricula,
         Boolean status,
-        @NotBlank(message = "O turno é obrigatório")
+        @NotNull(message = "O turno é obrigatório")
         Turno turno,
         Long pessoaId
 
