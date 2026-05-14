d29d7e3c3f16c6ed48dbac582d23dd506c713f65
David Belfort <davidedysonn@gmail.com>
2026-05-13 21:57:37 -0300

    fix(funcionarios): resposta do json era devolvida sem o id da pessoa vinculada ao funcionario.

--- src/main/java/com/funcionario/mapper/FuncionarioMapper.java
+++ src/main/java/com/funcionario/mapper/FuncionarioMapper.java
@@ -3,9 +3,13 @@ package com.funcionario.mapper;
 import com.funcionario.dtos.FuncionarioDTO;
 import com.funcionario.entities.Funcionario;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "cdi")
 public interface FuncionarioMapper {
+    @Mapping(target = "pessoa", ignore = true)
     Funcionario toEntity (FuncionarioDTO funcionarioDTO);
+    
+    @Mapping(source = "pessoa.id", target = "pessoaId")
     FuncionarioDTO toDTO (Funcionario funcionario);
 }
