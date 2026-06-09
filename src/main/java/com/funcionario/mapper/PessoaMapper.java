6958ef3effaeda33985edaf42d6682bbd0fbdbf8
David Belfort <davidedysonn@gmail.com>
2026-06-08 22:41:46 -0300

    fix(PessoaMapper): Ao fazer a conversao de DTO para Entidade o campo FUNCIONARIOS sera ignorado ja que nao faz parte da logica.

--- src/main/java/com/funcionario/mapper/PessoaMapper.java
+++ src/main/java/com/funcionario/mapper/PessoaMapper.java
@@ -3,9 +3,11 @@ package com.funcionario.mapper;
 import com.funcionario.dtos.PessoaDTO;
 import com.funcionario.entities.Pessoa;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "cdi")
 public interface PessoaMapper {
+    @Mapping(target = "funcionarios", ignore = true)
     Pessoa toEntity (PessoaDTO pessoaDTO);
     PessoaDTO toDTO (Pessoa pessoa);
 }
